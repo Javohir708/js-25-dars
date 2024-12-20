d1197166383dfd4bbf27f7cc65cc965dcb7f95a6
Javohir708 <joxauzking@gamil.com>
2024-12-20 09:06:16 +0500

    xatoliklar tuzatildi

--- JavaScript/product.js
+++ JavaScript/product.js
@@ -1,13 +1,13 @@
-const productImage = document.querySelector(".product__image");
-const productTitle = document.querySelector(".product__title");
-const productDescription = document.querySelector(".product__description");
-const productPrice = document.querySelector(".product__price");
+const productImage = document.querySelector(".product__image")
+const productTitle = document.querySelector(".product__title")
+const productDescription = document.querySelector(".product__description")
+const productPrice = document.querySelector(".product__price")
 
-const BASE_URL = "https://dummyjson.com";
+const BASE_URL = "https://dummyjson.com"
 
 function fetchProduct() {
-    const params = new URLSearchParams(window.location.search);
-    const productId = params.get("id");
+    const params = new URLSearchParams(window.location.search)
+    const productId = params.get("id")
 
     fetch(`${BASE_URL}/products/${productId}`)
         .then(response => response.json())
@@ -24,8 +24,8 @@ window.onload = ()=> {
 }
 
 function createDetailPage(product) {
-    productImage.src = product.thumbnail;
-    productTitle.textContent = product.title;
-    productDescription.textContent = product.description;
-    productPrice.textContent = `${product.price}$`;
+    productImage.src = product.thumbnail
+    productTitle.textContent = product.title
+    productDescription.textContent = product.description
+    productPrice.textContent = `${product.price}$`
 }
